ad9b37893d18527148c098250a2e35c75378395d
Herbert Lange <lange@ids-mannheim.de>
2023-02-09 17:15:05 +0100

    extend draft record to be more usable in api calls

--- src/main/java/de/idsmannheim/lza/inveniojavaapi/DraftRecord.java
+++ src/main/java/de/idsmannheim/lza/inveniojavaapi/DraftRecord.java
@@ -8,13 +8,16 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import de.idsmannheim.lza.inveniojavaapi.deserializers.DraftRecordDeserializer;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  *
  * @author Herbert Lange <lange@ids-mannheim.de>
  * 
- * A class combining Access, FilesOptions and Metadata
+ * A class combining Access, FilesOptions and Metadata as well as other information
  */
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @JsonDeserialize(using = DraftRecordDeserializer.class)
@@ -22,23 +25,103 @@ public class DraftRecord {
     
     @JsonProperty("access")
     Access access;
+    @JsonProperty("created")
+    Optional<Date> created = Optional.empty();
+    @JsonProperty("expires_at")
+    Optional<Date> expiresAt = Optional.empty();
     @JsonProperty("files")
     FilesOptions files;
+    @JsonProperty("id")
+    Optional<String> id = Optional.empty();
+    @JsonProperty("is_published")
+    Optional<Boolean> isPublished = Optional.empty();
+    @JsonProperty("links")
+    HashMap<String,String> links = new HashMap<>();
     @JsonProperty("metadata")
     Metadata metadata;
-
+    @JsonProperty("parent")
+    Optional<Record.Parent> parent = Optional.empty();
+    @JsonProperty("pids")
+    HashMap<String,ExternalPid> pids = new HashMap<>();
+    @JsonProperty("revision_id")
+    Optional<Integer> revisionId = Optional.empty();
+    @JsonProperty("updated")
+    Optional<Date> updated = Optional.empty();
+    @JsonProperty("versions")
+    Optional<Record.Versions> versions = Optional.empty();
+    
     public DraftRecord(Access access, FilesOptions files, Metadata metadata) {
         this.access = access;
         this.files = files;
         this.metadata = metadata;
     }
 
+    public DraftRecord setCreated(Date created) {
+        this.created = Optional.of(created);
+        return this;
+    }
+
+    public DraftRecord setExpiresAt(Date expiresAt) {
+        this.expiresAt = Optional.of(expiresAt);
+        return this;
+    }
+
+    public DraftRecord setId(String id) {
+        this.id = Optional.of(id);
+        return this;
+    }
+
+    public DraftRecord setIsPublished(Boolean isPublished) {
+        this.isPublished = Optional.of(isPublished);
+        return this;
+    }
+
+    public DraftRecord setLinks(HashMap<String, String> links) {
+        this.links.putAll(links);
+        return this;
+    }
+
+    public DraftRecord setParent(Record.Parent parent) {
+        this.parent = Optional.of(parent);
+        return this;
+    }
+
+    public DraftRecord setPids(HashMap<String,ExternalPid> pids) {
+        this.pids.putAll(pids);
+        return this;
+    }
+    
+    public DraftRecord setRevisionId(Integer revisionId) {
+        this.revisionId = Optional.of(revisionId);
+        return this;
+    }
+
+    public DraftRecord setUpdated(Date updated) {
+        this.updated = Optional.of(updated);
+        return this;
+    }
+    
+    public DraftRecord setVersions(Record.Versions versions) {
+        this.versions = Optional.of(versions);
+        return this;
+    }
+
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 17 * hash + Objects.hashCode(this.access);
-        hash = 17 * hash + Objects.hashCode(this.files);
-        hash = 17 * hash + Objects.hashCode(this.metadata);
+        hash = 19 * hash + Objects.hashCode(this.access);
+        hash = 19 * hash + Objects.hashCode(this.created);
+        hash = 19 * hash + Objects.hashCode(this.expiresAt);
+        hash = 19 * hash + Objects.hashCode(this.files);
+        hash = 19 * hash + Objects.hashCode(this.id);
+        hash = 19 * hash + Objects.hashCode(this.isPublished);
+        hash = 19 * hash + Objects.hashCode(this.links);
+        hash = 19 * hash + Objects.hashCode(this.metadata);
+        hash = 19 * hash + Objects.hashCode(this.parent);
+        hash = 19 * hash + Objects.hashCode(this.pids);
+        hash = 19 * hash + Objects.hashCode(this.revisionId);
+        hash = 19 * hash + Objects.hashCode(this.updated);
+        hash = 19 * hash + Objects.hashCode(this.versions);
         return hash;
     }
 
@@ -57,16 +140,52 @@ public class DraftRecord {
         if (!Objects.equals(this.access, other.access)) {
             return false;
         }
+        if (!Objects.equals(this.created, other.created)) {
+            return false;
+        }
+        if (!Objects.equals(this.expiresAt, other.expiresAt)) {
+            return false;
+        }
         if (!Objects.equals(this.files, other.files)) {
             return false;
         }
-        return Objects.equals(this.metadata, other.metadata);
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.isPublished, other.isPublished)) {
+            return false;
+        }
+        if (!Objects.equals(this.links, other.links)) {
+            return false;
+        }
+        if (!Objects.equals(this.metadata, other.metadata)) {
+            return false;
+        }
+        if (!Objects.equals(this.parent, other.parent)) {
+            return false;
+        }
+        if (!Objects.equals(this.pids, other.pids)) {
+            return false;
+        }
+        if (!Objects.equals(this.revisionId, other.revisionId)) {
+            return false;
+        }
+        if (!Objects.equals(this.updated, other.updated)) {
+            return false;
+        }
+        return Objects.equals(this.versions, other.versions);
     }
+
+    
 
     @Override
     public String toString() {
-        return "DraftRecord{" + "access=" + access + ", files=" + files + ", metadata=" + metadata + '}';
+        return "DraftRecord{" + "access=" + access + ", created=" + created + ", expiresAt=" + expiresAt + ", files=" + files + ", id=" + id + ", isPublished=" + isPublished + ", links=" + links + ", metadata=" + metadata + ", parent=" + parent + ", pids=" + pids + ", revisionId=" + revisionId + ", updated=" + updated + ", versions=" + versions + '}';
     }
+
+
+
+    
     
     
 }
