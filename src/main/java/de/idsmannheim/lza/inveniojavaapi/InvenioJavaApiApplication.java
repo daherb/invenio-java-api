928e41110c9fa443a3813c1130cb3f774f655dd5
Herbert Lange <lange@ids-mannheim.de>
2023-02-23 12:10:39 +0100

    remove some older test methods

--- src/main/java/de/idsmannheim/lza/inveniojavaapi/InvenioJavaApiApplication.java
+++ src/main/java/de/idsmannheim/lza/inveniojavaapi/InvenioJavaApiApplication.java
@@ -33,25 +33,9 @@ public class InvenioJavaApiApplication {
         String host = "repos-devel2.ids-mannheim.de:5000";
         String token = "voCZ7NcC0lwmCmluJlxhW5m1BYJjKQoHBIyxgmabuJCyEIzsFG6yE7JHwxe8";
         API api = new API(host, token);
-        APITools tools = new APITools(api);
-        // createTestData(API api);
-        // Delete all draft records
-//                Records records = API.listUserRecords();
-//                for (String id : records.hits.hits.stream().filter((r) -> r.status.equals("draft")).map((r) -> r.id).collect(Collectors.toList())) {
-//                    LOG.info("Trying to delete " + id);
-//                    API.deleteDraftRecord(id);
-//                }
-//                LOG.info(om.writeValueAsString(API.searchRecords(Optional.empty(),Optional.empty(),Optional.empty(),Optional.empty(),Optional.empty())));
-//                LOG.info(om.writeValueAsString(API.searchRecords(Optional.of("metadata.creators:Blank"),Optional.empty(),Optional.empty(),Optional.empty(),Optional.empty())));
-//                LOG.info(API.listDraftFiles("6k6r1-hpf49")));
-// createTestData();
-// LOG.info(CMDI.readCmdiMetadata(new SAXBuilder().build(new File("/home/herb/projekte/Metadata-i5-to-CMDI_Wikipedia-de/wdd17.cmdi"))).toString());
-// CMDI.readCmdiMetadata(new SAXBuilder().build(new File("/home/herb/projekte/Metadata-i5-to-CMDI_Wikipedia-de/wdd17.cmdi")));
-        // tools.deleteAllDrafts();
-        String id = tools.uploadDraftSip(new File("/home/herb/projekte/wikipedia-mini-test-sips/wdd13").toPath());
-        LOG.info(id);
-        System.out.println("Press key");System.in.read();
-        // tools.deleteDraftSip(id);
+        createTestData(api);
+//        demo(tools);
+        //versionedUpload(api, tools);
     }
     
     /**
@@ -93,76 +77,76 @@ public class InvenioJavaApiApplication {
         
     }
     
-    public static void demo(APITools tools) throws IOException, JDOMException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, InterruptedException, FileNotFoundException, JsonProcessingException, UnsupportedEncodingException {
-        LOG.info("Starting demo");
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Uploading draft SIP");
-        String id = tools.uploadDraftSip(new File("/home/herb/projekte/wikipedia-mini-test-sips/wdd13").toPath(), true);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.log(Level.INFO, "Downloading draft SIP {0}", id);
-        tools.downloadDraftSip(id, Path.of("/tmp", "drafts"));
-        System.out.println("Press key");
-        System.in.read();
-        LOG.log(Level.INFO, "Delete draft SIP {0}", id);
-        tools.deleteDraftSip(id);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Upload several SIPS");
-        Instant start = Instant.now();
-        tools.uploadDraftSips(new File("/home/herb/projekte/wikipedia-mini-test-sips").toPath(), true);
-        Instant end = Instant.now();
-        LOG.log(Level.INFO, "Took {0} seconds", end.getEpochSecond() - start.getEpochSecond());
-        LOG.info("Done");
-    }
-    
-    public static void versionedUpload(API api, APITools tools) throws IOException, JDOMException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, InterruptedException, FileNotFoundException, JsonProcessingException, UnsupportedEncodingException {
-         ObjectMapper om = new ObjectMapper();
-        om.findAndRegisterModules();
-        om.enable(SerializationFeature.INDENT_OUTPUT);
-        LOG.info("Starting upload");
-        LOG.info("Upload version 1");
-        String id = tools.uploadDraftSip(Path.of("/home/herb/projekte/test-sips/disko/sip1"), false);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Publish version 1");
-        tools.publishDraftSip(id);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Upload version 2");
-        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip2"));
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Publish version 2");
-        tools.publishDraftSip(id);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Upload version 3");
-        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip3"));
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Publish version 3");
-        tools.publishDraftSip(id);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Upload version 4");
-        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip4"));
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Publish version 4");
-        tools.publishDraftSip(id);
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Upload version 5");
-        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip5"));
-        System.out.println("Press key");
-        System.in.read();
-        LOG.info("Publish version 5");
-        tools.publishDraftSip(id);
-        System.out.println("Press key");
-        System.in.read();
-    }
+//    public static void demo(APITools tools) throws IOException, JDOMException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, InterruptedException, FileNotFoundException, JsonProcessingException, UnsupportedEncodingException {
+//        LOG.info("Starting demo");
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Uploading draft SIP");
+//        String id = tools.uploadDraftSip(new File("/home/herb/projekte/test-sips/wikipedia-mini-test-sips/wdd13").toPath(), true);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.log(Level.INFO, "Downloading draft SIP {0}", id);
+//        tools.downloadDraftSip(id, Path.of("/tmp", "drafts"));
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.log(Level.INFO, "Delete draft SIP {0}", id);
+//        tools.deleteDraftSip(id);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Upload several SIPS");
+//        Instant start = Instant.now();
+//        tools.uploadDraftSips(new File("/home/herb/projekte/test-sips/wikipedia-mini-test-sips").toPath(), true);
+//        Instant end = Instant.now();
+//        LOG.log(Level.INFO, "Took {0} seconds", end.getEpochSecond() - start.getEpochSecond());
+//        LOG.info("Done");
+//    }
+//    
+//    public static void versionedUpload(API api, APITools tools) throws IOException, JDOMException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, InterruptedException, FileNotFoundException, JsonProcessingException, UnsupportedEncodingException {
+//         ObjectMapper om = new ObjectMapper();
+//        om.findAndRegisterModules();
+//        om.enable(SerializationFeature.INDENT_OUTPUT);
+//        LOG.info("Starting upload");
+//        LOG.info("Upload version 1");
+//        String id = tools.uploadDraftSip(Path.of("/home/herb/projekte/test-sips/disko/sip1"), false);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Publish version 1");
+//        tools.publishDraftSip(id);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Upload version 2");
+//        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip2"));
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Publish version 2");
+//        tools.publishDraftSip(id);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Upload version 3");
+//        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip3"));
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Publish version 3");
+//        tools.publishDraftSip(id);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Upload version 4");
+//        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip4"));
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Publish version 4");
+//        tools.publishDraftSip(id);
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Upload version 5");
+//        id = tools.updateSip(id,Path.of("/home/herb/projekte/test-sips/disko/sip5"));
+//        System.out.println("Press key");
+//        System.in.read();
+//        LOG.info("Publish version 5");
+//        tools.publishDraftSip(id);
+//        System.out.println("Press key");
+//        System.in.read();
+//    }
     private static final Logger LOG = Logger.getLogger(InvenioJavaApiApplication.class.getName());
     
 }
