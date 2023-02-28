f83969f6a06af8f27f0530f4fe51b686e74503ba
Herbert Lange <lange@ids-mannheim.de>
2023-02-28 14:17:50 +0100

    add script to install pom

---
+++ install-pom.sh
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=target/invenio-java-api-0.0.1-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=invenio-java-api -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
