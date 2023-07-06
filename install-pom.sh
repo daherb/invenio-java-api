f713e11775f48467ea0b6432e3b0463afad590ac
Herbert Lange <lange@ids-mannheim.de>
2023-07-06 12:16:41 +0200

    add compile command

--- install-pom.sh
+++ install-pom.sh
@@ -1,1 +1,2 @@
+mvn clean compile package install
 mvn install:install-file -Dfile=target/invenio-java-api-0.0.1-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=invenio-java-api -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
