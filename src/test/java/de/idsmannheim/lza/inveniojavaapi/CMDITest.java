07047f36ac84e62acd0dec9048091a58cb3b60a5
Herbert Lange <lange@ids-mannheim.de>
2023-07-10 18:15:06 +0200

    unused import

--- src/test/java/de/idsmannheim/lza/inveniojavaapi/CMDITest.java
+++ src/test/java/de/idsmannheim/lza/inveniojavaapi/CMDITest.java
@@ -6,7 +6,6 @@ package de.idsmannheim.lza.inveniojavaapi;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import de.idsmannheim.lza.inveniojavaapi.cmdi.CollectionProfileMapper;
 import de.idsmannheim.lza.inveniojavaapi.cmdi.OLACDcmiTermsMapper;
 import de.idsmannheim.lza.inveniojavaapi.cmdi.SpeechCorpusProfileMapper;
