06147e1973fffc432476f001812e892691324a0c
Herbert Lange <lange@ids-mannheim.de>
2023-09-04 14:56:32 +0200

    use maven wrapper

--- install-pom.sh
+++ install-pom.sh
@@ -1,2 +1,2 @@
-mvn clean compile package install
-mvn install:install-file -Dfile=target/invenio-java-api-0.0.1-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=invenio-java-api -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
+./mvnw clean compile package install
+./mvnw install:install-file -Dfile=target/invenio-java-api-0.0.1-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=invenio-java-api -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
