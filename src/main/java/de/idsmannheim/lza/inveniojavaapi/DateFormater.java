9e9df94f5f109233883b6fc9acbd56884b7a8e7c
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2023-09-28 10:10:17 +0200

    make date parsing more robust by supporting multiple formats

--- src/main/java/de/idsmannheim/lza/inveniojavaapi/DateFormater.java
+++ src/main/java/de/idsmannheim/lza/inveniojavaapi/DateFormater.java
@@ -6,7 +6,9 @@ package de.idsmannheim.lza.inveniojavaapi;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * SimpleDateFormat used for parsing and formatting all dates in the API
@@ -16,10 +18,18 @@ public class DateFormater{
 
     private static DateFormater INSTANCE;
     
-    private final SimpleDateFormat dateFormat;
+    private final List<SimpleDateFormat> dateFormat = new ArrayList();
     
     private DateFormater() {
-        dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX");
+        
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss.SSSSSSXXX"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ssXXX"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss.SSSSSS"));
+        dateFormat.add(new SimpleDateFormat("yyyy-MM-dd' 'HH:mm:ss"));
     }
     
     /**
@@ -39,21 +49,23 @@ public class DateFormater{
      * @throws ParseException 
      */
     public Date parse(String s) throws ParseException {
-        try {
-            return dateFormat.parse(s);
+        for (SimpleDateFormat formater : dateFormat) {
+            try {
+                return formater.parse(s);
+            }
+            catch (ParseException e) {
+                // Do nothing, try the next format
+            }
         }
-        catch (ParseException e) {
-            // Try to fix the date
-            return dateFormat.parse(s.replace(' ', 'T') + "+00:00");
-        }
+        throw new ParseException("Unparsable date: "+ s, 0);
     }
     
     /**
-     * Same as SimpleDateFormater.format
+     * Same as SimpleDateFormater.format for the first format in the list of possible formats
      * @param d the Date to be formatted
      * @return the date formatted as string
      */
     public String format(Date d) {
-        return dateFormat.format(d);
+        return dateFormat.get(0).format(d);
     }
 }
