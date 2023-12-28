51cc2b5dabcd0c15484cedbd6fbf3f858f0737f3
Herbert Lange <github@pietaetskirsche.de>
2023-12-28 23:01:19 +0100

    fix module info

--- src/main/java/de/idsmannheim/lza/inveniojavaapi/invenio.java.api/module-info.java
+++ src/main/java/de/idsmannheim/lza/inveniojavaapi/invenio.java.api/module-info.java
@@ -2,7 +2,8 @@ module invenio.java.api {
     requires java.logging;
     requires java.net.http;
     requires java.xml;
-    
+    requires java.xml.bind;
+    requires java.activation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jdk8;
@@ -12,10 +13,11 @@ module invenio.java.api {
     requires spring.boot.autoconfigure;
     requires commons.vfs2;
     requires Saxon.HE;
-//    requires java.xml.bind;
+    requires xml.magic;
 
     exports de.idsmannheim.lza.inveniojavaapi;
     exports de.idsmannheim.lza.inveniojavaapi.cmdi;
     exports de.idsmannheim.lza.inveniojavaapi.deserializers;
 
+    opens de.idsmannheim.lza.inveniojavaapi to com.fasterxml.jackson.databind;
 }
